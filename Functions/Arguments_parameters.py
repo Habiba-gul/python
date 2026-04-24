c72c55adb363fbd94e3aef4afb0e0d1edf45e43d
Habiba-gul <hgull9712@gmail.com>
2026-04-24 13:22:41 +0500

    Arguments and parameters in a function

---
+++ Functions/Arguments_parameters.py
@@ -0,0 +1,10 @@
+# parameters : input define for function
+# argument: actual value for given parameter
+
+def greet(F_name,L_name):
+    print(f"hi {F_name} {L_name}")
+    print("welcome back")
+
+
+    #  function call
+greet("habiba","gul")
