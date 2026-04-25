a21eab7e121f9a9ab49081c9d614eea6d04a179f
Habiba-gul <hgull9712@gmail.com>
2026-04-25 11:18:43 +0500

    instance variable and class variable in python oop

--- instanceVariable_ Classvariable.py
+++ instanceVariable_ Classvariable.py
@@ -1,1 +1,6 @@
-# intance var use and defined i --init-- ,self
+# intance var use and defined i --init-- ,self belong to specific object unique per object
+# class var defined outside init share able to all obj
+# init runs automatically
+# self ----- current object
+# instance var unique per
+# method inside class opertae on onject data
